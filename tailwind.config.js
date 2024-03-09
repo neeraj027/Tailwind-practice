eec8e89ad4eae85cc1bcd399baaf835185893810
neeraj027 <neerajkshatriy@gmail.com>
2024-03-10 00:15:40 +0530

    Final commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "gray2": "#464445",
+        "yellow2": "#E5F243"
+      },
+      fontFamily: {
+        "Poppins": ["Poppins"]
+      },
+    },
+  },
+  plugins: [],
+}
+
